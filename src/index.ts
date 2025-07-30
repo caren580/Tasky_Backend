f573c0d346939e0cf1a5e6709c639f503e4117f1
caren580 <chumacaren69@gmail.com>
2025-07-30 03:03:25 +0300

    solve

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(cors({
     
     const allowedOrigins = [
       'http://localhost:5173',
-      'http://localhost:3000',
+      // 'http://localhost:3000',
       'https://tasky-frontend-ai9y.vercel.app',
       "https://tasky-frontend-s2mq-6tj5dpzqw-caren580s-projects.vercel.app",
       // '*',
